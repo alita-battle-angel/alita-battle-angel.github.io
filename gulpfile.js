d4e32b3379a6339465192cc7bf314ced5a4ff0db
Eeliya <eeliya.rasta@gmail.com>
2019-05-13 12:23:18 +0200

    Add scss support

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const sourcemaps = require('gulp-sourcemaps');
 const babel = require('gulp-babel');
 const concat = require('gulp-concat');
 const del = require('del');
+const sass = require('gulp-sass');
 
 const destination = 'public';
 
@@ -18,13 +19,22 @@ const copy = function (done) {
   gulp.src('src/images/*')
     .pipe(gulp.dest(destination + '/images'));
 
-  gulp.src('src/styles/*')
+  gulp.src('src/styles/**/*.css')
+    .pipe(gulp.dest(destination + '/styles'));
+  return done();
+};
+
+const scss = function (done) {
+  gulp.src('src/styles/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest(destination + '/styles'));
   return done();
 };
 
 const build = function (done) {
-  return gulp.src('src/**/*.js')
+  gulp.src('src/**/*.js')
     .pipe(sourcemaps.init())
     .pipe(babel())
     .pipe(concat('bundle.js'))
@@ -34,17 +44,20 @@ const build = function (done) {
 };
 
 const buildProd = function (done) {
-  return gulp.src('src/**/*.js')
+  gulp.src('src/**/*.js')
     .pipe(babel())
     .pipe(concat('bundle.js'))
     .pipe(gulp.dest(destination));
+
+  gulp.src('src/styles/**/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest(destination + '/styles'));
+
   return done();
 };
 
-// gulp.task('build-development', gulp.series(copy, build));
-
 gulp.task('build-production', gulp.series(clean, copy, buildProd));
 
-gulp.task('watch', gulp.series(clean, copy, build, function watching() {
-  gulp.watch('src/**/*.*', gulp.series(copy, build));
+gulp.task('watch', gulp.series(clean, copy, scss, build, function watching() {
+  gulp.watch('src/**/*.*', gulp.series(copy, scss, build));
 }));
