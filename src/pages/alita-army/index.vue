52c7b111e05581afcf6e7171d0002183d8b6c05a
Eeliya <eeliya.rasta@gmail.com>
2019-07-09 14:29:59 +0200

    Fix a link on Alita Army page.

--- src/pages/alita-army/index.vue
+++ src/pages/alita-army/index.vue
@@ -19,7 +19,7 @@
             Because "Alita Army" is a passionate fanbase of Alita's fans that positively support her. We will give our
             heart to her because she would do the same. Check out the <a
               class="link-white"
-              href="https://alitastan.fandom.com/wiki/Alitastan"
+              href="https://alitastan.fandom.com/wiki/The_Alita_Army"
             >
               The Alita Army
             </a> from Alitastan Wiki.
